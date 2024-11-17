4f86a6f01118a1a29deb209591d9b90050800042
pixel fox <the.pixelfox@outlook.com>
2024-11-17 22:46:48 +0330

    feat(todobodyalltodos.vue): add todo body for  all todos

---
+++ src/components/TodoBodyAllTodos.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useStore } from "../stores/todos";
+import EmptyBoxTodos from "./EmptyBoxTodos.vue";
+import TodoBox from "./TodoBox.vue";
+
+const { todos, removeTodo } = useStore();
+
+const handleRemoveTodo = (id: number | undefined) => {
+  removeTodo(id);
+};
+</script>
+
+<template>
+  <div class="w-full h-full flex flex-col justify-center items-center gap-3">
+    <EmptyBoxTodos v-if="todos.length === 0" />
+    <TodoBox
+      v-else
+      v-for="todo in todos"
+      :key="todo.id"
+      :text="todo.text"
+      :id="todo.id"
+      v-model:model-value="todo.complated"
+      @set-id="(id) => handleRemoveTodo(id)"
+    />
+  </div>
+</template>
