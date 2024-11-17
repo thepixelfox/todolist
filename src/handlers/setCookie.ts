64ce50e7b9ef3b0703ab95b11e1667320e69612c
pixel fox <the.pixelfox@outlook.com>
2024-11-17 23:47:18 +0330

    feat(setCookie.ts): add set cookie handle for add new cookie

---
+++ src/handlers/setCookie.ts
@@ -0,0 +1,10 @@
+function setCookie(name: string, value: string, days: number): void {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${encodeURIComponent(
+    value
+  )};${expires};path=/;SameSite=None; Secure`;
+}
+
+export default setCookie;
