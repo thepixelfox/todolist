675bd3cb93a1ef6e69bdc9adb64fa93e7ea85a48
pixel fox <the.pixelfox@outlook.com>
2024-11-17 23:48:45 +0330

    refactor(todos.ts): add persist todos array by cookie

--- src/stores/todos.ts
+++ src/stores/todos.ts
@@ -1,5 +1,7 @@
 import { defineStore } from "pinia";
 import { useToast } from "vue-toastification";
+import getCookie from "../handlers/getCookie";
+import setCookie from "../handlers/setCookie";
 
 export interface todosProps {
   id: number;
@@ -7,19 +9,30 @@ export interface todosProps {
   complated: boolean;
 }
 
+const handleGetTodos = () => {
+  const cookieTodos = getCookie("todos");
+  if (cookieTodos) {
+    return JSON.parse(cookieTodos) as todosProps[];
+  } else {
+    setCookie("todos", JSON.stringify([]), 30);
+    return [] as todosProps[];
+  }
+};
+
 export const useStore = defineStore("todos", {
   // arrow function recommended for full type inference
   state: () => {
     return {
       // all these properties will have their type inferred automatically
-      todos: [] as todosProps[],
+      todos: handleGetTodos(),
     };
   },
   actions: {
     addTodos(todo: todosProps) {
       const toast = useToast();
       this.todos.push(todo);
-      toast.success("add todo successfully!");
+      setCookie("todos", JSON.stringify(this.todos), 30);
+      toast.success("Add Todo Successfully!");
     },
     removeTodo(id: number | undefined) {
       const toast = useToast();
@@ -32,9 +45,10 @@ export const useStore = defineStore("todos", {
       const index = this.todos.findIndex((t) => t.id === id);
       if (index !== -1) {
         this.todos.splice(index, 1);
-        toast.success("Todo removed successfully!");
+        setCookie("todos", JSON.stringify(this.todos), 30);
+        toast.success("Todo Removed Successfully!");
       } else {
-        toast.error("Todo not found!");
+        toast.error("Todo Not Found!");
       }
     },
   },
