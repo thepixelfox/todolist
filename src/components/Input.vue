8c77504b24302958c0e9df541339925c0ad5df57
pixel fox <the.pixelfox@outlook.com>
2024-11-17 22:45:29 +0330

    feat(input.vue): add input component with dynamic prop and attr

---
+++ src/components/Input.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+});
+
+const props = defineProps({
+  modelValue: String,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const handleInput = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  const value = target.value;
+
+  emit("update:modelValue", value);
+};
+</script>
+
+<template>
+  <input
+    type="text"
+    class="bg-gray-custom-light w-full min-h-[3.5rem] border border-black-custom rounded-lg px-5 !outline-none !ring-0 placeholder:opacity-45"
+    placeholder="enter you todo text ...!"
+    :value="props.modelValue"
+    @input="handleInput"
+    v-bind="$attrs"
+  />
+</template>
