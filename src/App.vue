52ec85578b2a64618a2520154ebe5c9757081f4e
pixel fox <the.pixelfox@outlook.com>
2024-11-17 22:34:54 +0330

    feat(app.vue): add main component app vue file

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TodoBody from "./components/TodoBody.vue";
+import TodoHeader from "./components/TodoHeader.vue";
+</script>
+
+<template>
+  <div class="w-full h-full flex flex-col">
+    <TodoHeader />
+    <TodoBody />
+  </div>
+</template>
