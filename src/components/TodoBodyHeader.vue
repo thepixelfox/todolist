0880c9e20b644cfff6d0da623e97c51cd5d0631e
pixel fox <the.pixelfox@outlook.com>
2024-11-17 22:47:14 +0330

    feat(todobodyheader.vue): add todo body header component

---
+++ src/components/TodoBodyHeader.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useStore } from "../stores/todos";
+
+const { todos } = useStore();
+</script>
+
+<template>
+  <div class="w-full h-auto flex justify-between items-center">
+    <div class="w-auto gap-2 flex justify-center items-center">
+      <span class="text-blue-light">All Todos :</span>
+      <span class="px-2 rounded-full bg-gray-custom-light">{{
+        todos.length
+      }}</span>
+    </div>
+
+    <div class="w-auto gap-2 flex justify-center items-center">
+      <span class="text-blue-light">Complated :</span>
+      <div class="w-auto h-auto">
+        <span
+          class="px-2 rounded-full bg-gray-custom-light"
+          v-if="todos.filter((t) => t.complated).length === 0"
+          >0</span
+        >
+        <div
+          class="flex justify-center items-center gap-1 px-2 rounded-full bg-gray-custom-light"
+          v-else
+        >
+          <span>{{ todos.filter((t) => t.complated).length }}</span>
+          <span class="text-xs">from</span>
+          <span>{{ todos.length }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
