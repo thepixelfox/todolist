4be0cfef13ccb2ca0e07a56648b3257897909d54
pixel fox <the.pixelfox@outlook.com>
2024-11-17 22:45:55 +0330

    feat(todobody.vue): add TodoBody component

---
+++ src/components/TodoBody.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TodoBodyAllTodos from "./TodoBodyAllTodos.vue";
+import TodoBodyHeader from "./TodoBodyHeader.vue";
+</script>
+
+<template>
+  <div class="w-full max-w-custom-container mx-auto h-full mt-28 sm:mt-16 px-4">
+    <div class="w-full h-full flex flex-col justify-center items-center gap-6">
+      <TodoBodyHeader />
+      <TodoBodyAllTodos />
+    </div>
+  </div>
+</template>
